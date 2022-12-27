b024719ba9beb42278a8a495454b6ae4f4bce1e1
Ryu-dai <ryudaimisawa21@gmail.com>
2022-12-27 22:59:45 +0900

    pandas でcsvを読み込んで、outputに書き出す

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('this is a code in the branch called develop')
+import pandas as pd
+
+filepath = '/Users/ryudai/Desktop/data_sfc/A_01.csv'
+
+data = pd.read_csv(filepath)
+
+
+data.to_csv('/Users/ryudai/Desktop/output.csv')
