aa55672f30fa2249aa67f71adda163ffefee1722
Ryu-dai <ryudaimisawa21@gmail.com>
2022-12-28 11:31:18 +0900

    とりあえずクラスの途中だけどコミットします

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pandas as pd
 
-filepath = '/Users/ryudai/Desktop/data_sfc/A_02.csv'
+class Data1:
+    def __init__(self):
+        self.filepathroot = '/Users/ryudai/Desktop/data_sfc/A_01.csv'
+
+        self.filepath = self.filepathroot + self.filepath
+        pass
+
+class Data2:
+    def __init__(self):
+        self.filepath = '/Users/ryudai/Desktop/data_sfc/A_02.csv'
+        pass
+
+
 
 data = pd.read_csv(filepath)
 
-data60 = data.loc[ 0 : 59 , 'date':'steps']
+data60 = data.loc[ 0 : 59 , 'date' : 'steps' ]
+
+print(data60)
 
 data60.to_csv('/Users/ryudai/Desktop/output.csv')
